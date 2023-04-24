11668a5bee3491684af7c315e9ae1fa1baafc90e
inwpuun <112232858+inwpuun@users.noreply.github.com>
2023-04-24 18:08:17 +0700

    more readable path

--- routes/coursevilleRoutes.js
+++ routes/coursevilleRoutes.js
@@ -5,9 +5,16 @@ const router = express.Router();
 
 router.get("/auth_app", coursevilleController.authApp);
 router.get("/access_token", coursevilleController.accessToken);
+
+// สำหรับเอาไว้ดึงข้อมูลพวกชื่อของ user ที่ login แล้ว
 router.get("/get_profile_info", coursevilleController.getProfileInformation);
+
+//สำหรับดึงข้อมูลของ course ทั้งหมด ถ้าใส่ query string แล้วก็จะ filter ตาม query string นั้น มี year กับ semester
 router.get("/getAllAssignments", coursevilleController.getAllAssignments);
+
+//สำหรับดึงข้อมูลของ course ที่เลือก
 router.get("/getCourseAssignments/:cv_cid", coursevilleController.getCourseAssignments);
+
 router.get("/logout", coursevilleController.logout);
 
 module.exports = router;
